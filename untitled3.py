c245a12e774bfec2b6b55ebfa110006c819ce313
efekizik <147101551+efekizik@users.noreply.github.com>
2023-12-12 02:27:54 +0300

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 02:26:28 2023
+
+@author: Efe
+"""
+
+kadi=input("Kullanıcı adınızı giriniz: ")
+ksifre=int(input("Şifrenizi giriniz: "))
+
+if kadi=="Efe Kızık" and ksifre==1881:
+    print("Kullanıcı bilgileri doğru")
+else:
+    print("Kullanıcı bilgileri hatalı")
